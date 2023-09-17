e42a6a868d79540912f7d087d7b1604d1eaf00a7
toxirjon96 <tatu.toxirjon@gmail.com>
2023-09-17 23:49:49 +0500

    http request finished

--- lib/src/pages/home_page/home_page.dart
+++ lib/src/pages/home_page/home_page.dart
@@ -1,10 +1,63 @@
 import 'package:weather_app/weather_app_library.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Weather? weather;
+  Widget? activeScreen;
+  String stateOfApp = "Loading...";
+
+  final WeatherAppController<Weather> weatherAppController =
+      WeatherAppController(
+    convert: Weather.convert(),
+    requestRepository: HttpRequestService(),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    makeRequest();
+  }
+
+  void makeRequest() async {
+    try {
+      Weather w = await weatherAppController.get(
+          "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Toshkent?unitGroup=us&key=HX4CGDZRV4UEJURH9Z2ZQ4FUJ&contentType=json");
+
+      setState(() {
+        weather = w;
+      });
+    } on JsonDecodeException catch (e) {
+      setState(() {
+        stateOfApp = e.message;
+      });
+    } on HttpUrlException catch (e) {
+      setState(() {
+        stateOfApp = e.message;
+      });
+    } on HttpRequestException catch (e) {
+      setState(() {
+        stateOfApp = e.message;
+      });
+    } on HttpStatusCodeException catch (e) {
+      setState(() {
+        stateOfApp = e.message;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (weather != null) {
+      activeScreen = const ExpandedHomeBody();
+    } else {
+      activeScreen = Loading(stateOfApp: stateOfApp);
+    }
     return SafeArea(
       child: Scaffold(
         body: Container(
@@ -19,7 +72,29 @@ class HomePage extends StatelessWidget {
               ],
             ),
           ),
-          child: const ExpandedHomeBody(),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  const Loading({
+    super.key,
+    required this.stateOfApp,
+  });
+
+  final String stateOfApp;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        stateOfApp,
+        style: const TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
         ),
       ),
     );
